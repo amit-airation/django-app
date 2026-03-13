9ec1815a38a8a951b04cc833f6dbac8fe03359c1
Amit Verma <amitz.airation@gmail.com>
2026-03-13 14:41:37 +0530

    few dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -26,6 +26,7 @@ WORKDIR /app/url-shortener
 EXPOSE 8080
 
 # Run migrations and start server
-CMD python manage.py migrate && \
+CMD python manage.py makemigrations && \
+    python manage.py migrate && \
     python manage.py collectstatic --noinput && \
     gunicorn config.wsgi:application --bind 0.0.0.0:8080
--- url-shortener/config/settings.py
+++ url-shortener/config/settings.py
@@ -144,6 +144,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
